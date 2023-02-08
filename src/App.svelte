e281727af6e047251bc94885d5b5e8288b9dcb97
Baticaly <ta6nbe@gmail.com>
2023-02-08 23:18:44 +0100

    css media update

--- src/App.svelte
+++ src/App.svelte
@@ -21,4 +21,11 @@
   .logo:hover {
     filter: drop-shadow(0 0 4em #ff3e00aa);
   }
+
+  @media only screen and (max-width: 500px) {
+    .logo {
+      height: 8em;
+      padding: 1.5em;
+    }
+  }
 </style>
