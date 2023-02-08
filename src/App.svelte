56523fea83b770b1922649220f119d89aa7ea1fc
Baticaly <ta6nbe@gmail.com>
2023-02-08 23:00:30 +0100

    initial upload

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import cosmosLogo from "./assets/cosmos.svg";
+</script>
+
+<main>
+  <div>
+    <a href="https://cloud.cosmosteam.org" target="_blank" rel="noreferrer">
+      <img src={cosmosLogo} class="logo" alt="Cosmos Logo" />
+    </a>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 14em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+    cursor: pointer;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 4em #ff3e00aa);
+  }
+</style>
